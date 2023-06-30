d0dd3d7d6cb8a6f899829ff35533dc11e5279925
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-30 11:24:38 +0300

    Defined the InMemoryCatalog store

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -7,6 +7,26 @@ public class InMemItemsRepository
 {
     private readonly List<Item> items = new()
     {
-
+        new Item
+        {
+            Id = Guid.NewGuid(),
+            Name = "Potion",
+            Price = 9,
+            CreatedDate = DateTimeOffset.UtcNow
+        },
+        new Item
+        {
+            Id = Guid.NewGuid(),
+            Name = "Iron Sword",
+            Price = 20,
+            CreatedDate = DateTimeOffset.UtcNow
+        },
+        new Item
+        {
+            Id = Guid.NewGuid(),
+            Name = "Bronze Shield",
+            Price = 15,
+            CreatedDate = DateTimeOffset.UtcNow
+        },
     };
 }
