366777f7aff2ce61d4355da97bb9d91499b0f070
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-30 14:10:09 +0300

    Added and implemented the Interface

---
+++ Repositories/MongoDbItemsRepository.cs
@@ -0,0 +1,32 @@
+using Catalog.Entities;
+
+namespace Catalog.Repositories;
+
+
+public class MongoDbItemsRepository : IInMemItemsRepository
+{
+    public void CreateItem(Item item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteItem(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Item GetItem(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Item> GetItems()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateItem(Item item)
+    {
+        throw new NotImplementedException();
+    }
+}
